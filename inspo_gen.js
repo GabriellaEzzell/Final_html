706f5315baf2c6413b8041811ebf5f097f8891f5
GabriellaEzzell <gezzell05@gmail.com>
2019-12-17 12:21:44 -0700

    Final Project

--- inspo_gen.js
+++ inspo_gen.js
@@ -1,1 +1,19 @@
-alert("Do something great today!");
+alert("Do something great today!")
+
+function = buttgo.onclick() {
+    Math.floor(Math.random() * 7 + 1);
+    if 1 
+    "1".value
+    if 2
+    "2".value
+    if 3
+    "3".value
+    if 4
+    "4".value
+    if 5
+    "5".value
+    if 6
+    "6".value
+    if 7
+    "7".value
+}
